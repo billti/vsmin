d39dc09adb77d7d70c8116c7ce51389e0040f454
Bill Ticehurst <billti@hotmail.com>
2023-09-22 13:11:39 -0700

    Fixed trailing slash

--- extension.js
+++ extension.js
@@ -1,15 +1,16 @@
 //@ts-check
 
+console.log("In first line of vs-min extension")
 var vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context 
  */
-function activate(context) {
-    console.log('vsmin extension is active');
+async function activate(context) {
+    console.log('vs-min extension is active');
 
-    var disposable = vscode.commands.registerCommand('vsmin-cmd', function () {
-        vscode.window.showInformationMessage('Hello from extension vsmin!');
+    var disposable = vscode.commands.registerCommand('vsmin.cmd', function () {
+        vscode.window.showInformationMessage('Hello from extension vs-min!');
     });
 
     context.subscriptions.push(disposable);
